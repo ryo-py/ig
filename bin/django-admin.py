f5533e55232a05ffd5c609795b1efbed70b3bf4d
Ryo <ryo91.hb@gmail.com>
2021-05-30 22:37:29 +0900

    python-3.9.5 setup

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/iriekei/Desktop/django_idea/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ ig-django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ryo/Dev/IdeaGenerator-master/ig-django-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
